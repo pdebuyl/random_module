0c0c137b537822f66ac8740cbdfd4395866e480e
Pierre de Buyl <pdebuyl@pdebuyl.be>
2015-08-14 13:10:50 +0200

    use a struct for the state

--- mt19937ar/mt19937ar.c
+++ mt19937ar/mt19937ar.c
@@ -50,33 +50,40 @@
 #define UPPER_MASK 0x80000000UL /* most significant w-r bits */
 #define LOWER_MASK 0x7fffffffUL /* least significant r bits */
 
-static unsigned long mt[N]; /* the array for the state vector  */
-static int mti=N+1; /* mti==N+1 means mt[N] is not initialized */
+typedef struct mt19937ar_struct {
+  unsigned long mt[N]; /* the array for the state vector  */
+  int mti; /* mti==N+1 means mt[N] is not initialized */
+} mt19937ar_t;
+
 
 /* initializes mt[N] with a seed */
-void init_genrand(unsigned long s)
+void init_genrand(mt19937ar_t *state, unsigned long s)
 {
-    mt[0]= s & 0xffffffffUL;
-    for (mti=1; mti<N; mti++) {
-        mt[mti] = 
-	    (1812433253UL * (mt[mti-1] ^ (mt[mti-1] >> 30)) + mti); 
-        /* See Knuth TAOCP Vol2. 3rd Ed. P.106 for multiplier. */
-        /* In the previous versions, MSBs of the seed affect   */
-        /* only MSBs of the array mt[].                        */
-        /* 2002/01/09 modified by Makoto Matsumoto             */
-        mt[mti] &= 0xffffffffUL;
-        /* for >32 bit machines */
-    }
+  int i;
+  unsigned long *mt = state->mt;
+  mt[0]= s & 0xffffffffUL;
+  for (i=1; i<N; i++) {
+    mt[i] =
+      (1812433253UL * (mt[i-1] ^ (mt[i-1] >> 30)) + i);
+    /* See Knuth TAOCP Vol2. 3rd Ed. P.106 for multiplier. */
+    /* In the previous versions, MSBs of the seed affect   */
+    /* only MSBs of the array mt[].                        */
+    /* 2002/01/09 modified by Makoto Matsumoto             */
+    mt[i] &= 0xffffffffUL;
+    /* for >32 bit machines */
+  }
+  state->mti = i;
 }
 
 /* initialize by an array with array-length */
 /* init_key is the array for initializing keys */
 /* key_length is its length */
 /* slight change for C++, 2004/2/26 */
-void init_by_array(unsigned long init_key[], int key_length)
+void init_by_array(mt19937ar_t *state, unsigned long init_key[], int key_length)
 {
     int i, j, k;
-    init_genrand(19650218UL);
+    unsigned long *mt = state->mt;
+    init_genrand(state, 19650218UL);
     i=1; j=0;
     k = (N>key_length ? N : key_length);
     for (; k; k--) {
@@ -99,33 +106,33 @@ void init_by_array(unsigned long init_key[], int key_length)
 }
 
 /* generates a random number on [0,0xffffffff]-interval */
-unsigned long genrand_int32(void)
+unsigned long genrand_int32(mt19937ar_t *state)
 {
     unsigned long y;
-    static unsigned long mag01[2]={0x0UL, MATRIX_A};
+    const unsigned long mag01[2]={0x0UL, MATRIX_A};
     /* mag01[x] = x * MATRIX_A  for x=0,1 */
 
-    if (mti >= N) { /* generate N words at one time */
+    if (state->mti >= N) { /* generate N words at one time */
         int kk;
 
-        if (mti == N+1)   /* if init_genrand() has not been called, */
-            init_genrand(5489UL); /* a default initial seed is used */
+        if (state->mti == N+1)   /* if init_genrand() has not been called, */
+            init_genrand(state, 5489UL); /* a default initial seed is used */
 
         for (kk=0;kk<N-M;kk++) {
-            y = (mt[kk]&UPPER_MASK)|(mt[kk+1]&LOWER_MASK);
-            mt[kk] = mt[kk+M] ^ (y >> 1) ^ mag01[y & 0x1UL];
+            y = (state->mt[kk]&UPPER_MASK)|(state->mt[kk+1]&LOWER_MASK);
+            state->mt[kk] = state->mt[kk+M] ^ (y >> 1) ^ mag01[y & 0x1UL];
         }
         for (;kk<N-1;kk++) {
-            y = (mt[kk]&UPPER_MASK)|(mt[kk+1]&LOWER_MASK);
-            mt[kk] = mt[kk+(M-N)] ^ (y >> 1) ^ mag01[y & 0x1UL];
+            y = (state->mt[kk]&UPPER_MASK)|(state->mt[kk+1]&LOWER_MASK);
+            state->mt[kk] = state->mt[kk+(M-N)] ^ (y >> 1) ^ mag01[y & 0x1UL];
         }
-        y = (mt[N-1]&UPPER_MASK)|(mt[0]&LOWER_MASK);
-        mt[N-1] = mt[M-1] ^ (y >> 1) ^ mag01[y & 0x1UL];
+        y = (state->mt[N-1]&UPPER_MASK)|(state->mt[0]&LOWER_MASK);
+        state->mt[N-1] = state->mt[M-1] ^ (y >> 1) ^ mag01[y & 0x1UL];
 
-        mti = 0;
+        state->mti = 0;
     }
   
-    y = mt[mti++];
+    y = state->mt[state->mti++];
 
     /* Tempering */
     y ^= (y >> 11);
@@ -137,36 +144,36 @@ unsigned long genrand_int32(void)
 }
 
 /* generates a random number on [0,0x7fffffff]-interval */
-long genrand_int31(void)
+long genrand_int31(mt19937ar_t *state)
 {
-    return (long)(genrand_int32()>>1);
+    return (long)(genrand_int32(state)>>1);
 }
 
 /* generates a random number on [0,1]-real-interval */
-double genrand_real1(void)
+double genrand_real1(mt19937ar_t *state)
 {
-    return genrand_int32()*(1.0/4294967295.0); 
+    return genrand_int32(state)*(1.0/4294967295.0); 
     /* divided by 2^32-1 */ 
 }
 
 /* generates a random number on [0,1)-real-interval */
-double genrand_real2(void)
+double genrand_real2(mt19937ar_t *state)
 {
-    return genrand_int32()*(1.0/4294967296.0); 
+    return genrand_int32(state)*(1.0/4294967296.0); 
     /* divided by 2^32 */
 }
 
 /* generates a random number on (0,1)-real-interval */
-double genrand_real3(void)
+double genrand_real3(mt19937ar_t *state)
 {
-    return (((double)genrand_int32()) + 0.5)*(1.0/4294967296.0); 
+    return (((double)genrand_int32(state)) + 0.5)*(1.0/4294967296.0);
     /* divided by 2^32 */
 }
 
 /* generates a random number on [0,1) with 53-bit resolution*/
-double genrand_res53(void) 
+double genrand_res53(mt19937ar_t *state)
 { 
-    unsigned long a=genrand_int32()>>5, b=genrand_int32()>>6; 
+    unsigned long a=genrand_int32(state)>>5, b=genrand_int32(state)>>6; 
     return(a*67108864.0+b)*(1.0/9007199254740992.0); 
 } 
 /* These real versions are due to Isaku Wada, 2002/01/09 added */
@@ -175,15 +182,16 @@ int main(void)
 {
     int i;
     unsigned long init[4]={0x123, 0x234, 0x345, 0x456}, length=4;
-    init_by_array(init, length);
+    mt19937ar_t s;
+    init_by_array(&s, init, length);
     printf("1000 outputs of genrand_int32()\n");
     for (i=0; i<1000; i++) {
-      printf("%10lu ", genrand_int32());
+      printf("%10lu ", genrand_int32(&s));
       if (i%5==4) printf("\n");
     }
     printf("\n1000 outputs of genrand_real2()\n");
     for (i=0; i<1000; i++) {
-      printf("%10.8f ", genrand_real2());
+      printf("%10.8f ", genrand_real2(&s));
       if (i%5==4) printf("\n");
     }
     return 0;
