4815080398730406338b9a5466bae99a4d349701
Pierre de Buyl <pdebuyl@pdebuyl.be>
2015-08-26 18:29:03 +0200

    implement normal distribution and extend wrapped routines

--- mt19937ar/mt19937ar_module.f90
+++ mt19937ar/mt19937ar_module.f90
@@ -22,7 +22,10 @@ module mt19937ar_module
   implicit none
 
   private
-  public :: init_by_array, genrand_int32, genrand_int31, genrand_real1, init_genrand
+  public :: mt19937ar_t
+  public :: init_by_array, genrand_int32, genrand_int31, init_genrand
+  public :: genrand_real1, genrand_real2, genrand_real3
+  public :: mt_normal, mt_normal_data
 
   interface
      subroutine init_genrand(s, seed) bind(c, name='init_genrand')
@@ -57,6 +60,27 @@ module mt19937ar_module
      end function
   end interface
 
+  interface
+     real(c_double) function genrand_real2(s) bind(c, name='genrand_real2')
+       use iso_c_binding
+       use mt19937ar_type_module
+       type(mt19937ar_t) :: s
+     end function
+  end interface
+
+  interface
+     real(c_double) function genrand_real3(s) bind(c, name='genrand_real3')
+       use iso_c_binding
+       use mt19937ar_type_module
+       type(mt19937ar_t) :: s
+     end function
+  end interface
+
+  interface mt_normal_data
+     module procedure mt_normal_data_d_1
+     module procedure mt_normal_data_d_2
+  end interface mt_normal_data
+
 contains
 
   subroutine init_by_array(state, init)
@@ -75,5 +99,57 @@ contains
     call init_by_array_c(state, init, int(size(init), c_int))
 
   end subroutine init_by_array
+
+  !! Return a normal-distributed random variable
+  function mt_normal(state) result(data)
+    type(mt19937ar_t), intent(inout) :: state
+    double precision :: data
+
+    double precision :: u1, u2, radius
+    logical :: found
+
+    if ( state% has_gauss ) then
+       data = state% gauss
+       state% has_gauss = .false.
+    else
+       found = .false.
+       do while (.not. found)
+          u1 = 2*genrand_real1(state) - 1
+          u2 = 2*genrand_real1(state) - 1
+          radius = (u1**2+u2**2)
+          if ( ( radius < 1 ) .and. (radius > 0) ) found = .true.
+       end do
+       state% gauss = u1 * sqrt( -2 * log(radius)/radius )
+       state% has_gauss = .true.
+       data = u2 * sqrt( -2 * log(radius)/radius )
+    end if
+
+  end function mt_normal
+
+  subroutine mt_normal_data_d_1(data, state)
+    double precision, intent(inout) :: data(:)
+    type(mt19937ar_t), intent(inout) :: state
+
+    integer :: i
+
+    do i = 1, size(data, dim=1)
+       data(i) = mt_normal(state)
+    end do
+
+  end subroutine mt_normal_data_d_1
+
+  subroutine mt_normal_data_d_2(data, state)
+    double precision, intent(inout) :: data(:,:)
+    type(mt19937ar_t), intent(inout) :: state
+
+    integer :: i, j
+
+    do j = 1, size(data, dim=2)
+    do i = 1, size(data, dim=1)
+       data(i,j) = mt_normal(state)
+    end do
+    end do
+
+  end subroutine mt_normal_data_d_2
 
 end module mt19937ar_module
