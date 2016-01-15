02b41a5ad68222f14f58762f08b8fe76191e5ac9
Pierre de Buyl <pdebuyl@pdebuyl.be>
2016-01-15 13:34:00 +0100

    enable cmake for threefry and add int_double program

---
+++ threefry/int_double.c
@@ -0,0 +1,22 @@
+#include <stdint.h>
+#include <inttypes.h>
+#include <stdio.h>
+#include "threefry.h"
+
+int main(void) {
+
+  threefry_t p, k;
+  threefry_t c;
+
+  p.c0 = 0x0;
+  p.c1 = 0x0;
+  k.c0 = 0x0;
+  k.c1 = 0x0;
+
+  printf("%20.16g\n", threefry_double(&p, &k));
+  printf("%20.16g\n", threefry_double(&p, &k));
+  printf("%20.16g\n", threefry_double(&p, &k));
+  printf("%20.16g\n", threefry_double(&p, &k));
+
+  return 0;
+}
