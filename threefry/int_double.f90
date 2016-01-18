798e50774335aa7b7014fbb6968e4af9ead8c419
Pierre de Buyl <pdebuyl@pdebuyl.be>
2016-01-18 21:20:25 +0100

    add threefry_rng_t and normal dist

--- threefry/int_double.f90
+++ threefry/int_double.f90
@@ -3,16 +3,16 @@ program int_double
   use threefry_module
   implicit none
 
-  type(threefry_t) :: c, k
+  type(threefry_rng_t) :: s
 
-  c%c0 = 0
-  c%c1 = 0
-  k%c0 = 0
-  k%c1 = 0
+  s%counter%c0 = 0
+  s%counter%c1 = 0
+  s%key%c0 = 0
+  s%key%c1 = 0
 
-  write(*,'(f20.16)') threefry_double(c,k)
-  write(*,'(f20.16)') threefry_double(c,k)
-  write(*,'(f20.16)') threefry_double(c,k)
-  write(*,'(f20.16)') threefry_double(c,k)
+  write(*,'(f20.16)') threefry_double(s)
+  write(*,'(f20.16)') threefry_double(s)
+  write(*,'(f20.16)') threefry_double(s)
+  write(*,'(f20.16)') threefry_double(s)
 
 end program int_double
--- threefry/threefry_module.f90
+++ threefry/threefry_module.f90
@@ -4,11 +4,19 @@ module threefry_type_module
 
   private
   public :: threefry_t
+  public :: threefry_rng_t
 
   type, bind(c) :: threefry_t
      integer(c_long) :: c0
      integer(c_long) :: c1
   end type threefry_t
+
+  type :: threefry_rng_t
+     type(threefry_t) :: counter
+     type(threefry_t) :: key
+     double precision :: gauss
+     logical :: has_gauss
+  end type threefry_rng_t
 
 end module threefry_type_module
 
@@ -19,15 +27,52 @@ module threefry_module
 
   private
   public :: threefry_t
+  public :: threefry_rng_t
   public :: threefry_double
+  public :: threefry_normal
 
   interface
-     real(c_double) function threefry_double(c, k) bind(c, name='threefry_double')
+     real(c_double) function threefry_double_c(c, k) bind(c, name='threefry_double')
        use iso_c_binding
        use threefry_type_module
        type(threefry_t) :: c
        type(threefry_t) :: k
-     end function threefry_double
+     end function threefry_double_c
   end interface
+
+contains
+
+  !! Return a single [0,1[ double precision number
+  function threefry_double(state) result(data)
+    type(threefry_rng_t), intent(inout) :: state
+    double precision :: data
+    data = threefry_double_c(state%counter, state%key)
+  end function threefry_double
+
+  !! Return a normal-distributed random variable
+  function threefry_normal(state) result(data)
+    type(threefry_rng_t), intent(inout) :: state
+    double precision :: data
+
+    double precision :: u1, u2, radius
+    logical :: found
+
+    if ( state% has_gauss ) then
+       data = state% gauss
+       state% has_gauss = .false.
+    else
+       found = .false.
+       do while (.not. found)
+          u1 = 2*threefry_double_c(state%counter, state%key) - 1
+          u2 = 2*threefry_double_c(state%counter, state%key) - 1
+          radius = (u1**2+u2**2)
+          if ( ( radius < 1 ) .and. (radius > 0) ) found = .true.
+       end do
+       state% gauss = u1 * sqrt( -2 * log(radius)/radius )
+       state% has_gauss = .true.
+       data = u2 * sqrt( -2 * log(radius)/radius )
+    end if
+
+  end function threefry_normal
 
 end module threefry_module
